f5f11369b93c43d84d7f9f6ef2fc0b85a0cc36b5
mikofo <michaelfoster@humcapital.com>
2023-12-04 22:20:26 -0600

    finish day 4

---
+++ AoC/Day4/part1.js
@@ -0,0 +1,19 @@
+function getPoints(game) {
+  const sets = game
+    .split(":")[1]
+    .split("|")
+    .map((set) => set.match(/\d+/g));
+
+  const matches = sets[1].filter((num) => sets[0].includes(num));
+  if (matches.length === 0) return 0;
+  return Math.pow(2, matches.length - 1);
+}
+
+function solve(input) {
+  return input
+    .split("\n")
+    .filter((item) => item)
+    .reduce((acc, line, index) => acc + getPoints(line), 0);
+}
+
+module.exports = solve;
---
+++ AoC/Day4/part2.js
@@ -0,0 +1,26 @@
+function solve(input) {
+  const copies = {};
+  function checkScratchcards(game, gameIndex) {
+    copies[gameIndex] = copies[gameIndex] ? copies[gameIndex] + 1 : 1;
+    const sets = game
+      .split(":")[1]
+      .split("|")
+      .map((set) => set.match(/\d+/g));
+
+    const matches = sets[1].filter((num) => sets[0].includes(num));
+    matches.forEach((_, index) => {
+      const key = gameIndex + index + 1;
+      if (copies[key]) copies[key] += copies[gameIndex];
+      else copies[key] = copies[gameIndex];
+    });
+  }
+
+  input
+    .split("\n")
+    .filter((item) => item)
+    .forEach((line, index) => checkScratchcards(line, index + 1));
+
+  return Object.values(copies).reduce((acc, val) => acc + val, 0);
+}
+
+module.exports = solve;
